888e377222574470716f5e9999abaf4302051c14
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-07-31 22:25:32 -0400

    maybe done with models?

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,5 @@
+// config/connection.js
+
 const { connect, connection } = require("mongoose");
 
 connect("mongodb://127.0.0.1:27017/SocialNetwork_db", {
---
+++ models/index.js
@@ -0,0 +1,6 @@
+// models/index.js
+
+const User = require("./user");
+const Thought = require("./thought");
+
+module.exports = { User, Thought };
