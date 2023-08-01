1841b20679a6c7a178b910c9f3c7ff4f44e2f380
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-07-31 23:37:28 -0400

    routing is done maybe?

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,23 @@
 // routes/api/userRoutes.js
+
+const router = require("express").Router();
+
+const userController = require("../../controllers/userController");
+
+router
+  .route("/")
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route("/:userId")
+  .get(userController.getUserById)
+  .put(userController.updateUser)
+  .delete(userController.deleteUser);
+
+router
+  .route("/:userId/friends/:friendId")
+  .post(userController.addFriend)
+  .delete(userController.removeFriend);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,8 @@
 // routes/index.js
+
+const router = require("express").Router();
+const routes = require("./api");
+
+router.use("/api", routes);
+
+module.exports = router;
